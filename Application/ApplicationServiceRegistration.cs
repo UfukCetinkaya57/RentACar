d8aa79cab23aa10246564b1d16342df2af86717c
Ufuk <Ufuk@DESKTOP-8T9IQ2C>
2024-08-26 23:49:17 +0300

    Updated Success

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,11 +1,12 @@
 ﻿//using Core.Application.Pipelines.Caching;
 //using Core.Application.Pipelines.Logging;
 //using Core.Application.Pipelines.Transaction;
-//using Core.Application.Pipelines.Validation;
-//using Core.Application.Rules;
+using Core.Application.Pipelines.Validation;
+using Core.Application.Rules;
 //using Core.CrossCuttingConcerns.Serilog;
 //using Core.CrossCuttingConcerns.Serilog.Logger;
-//using FluentValidation;
+using FluentValidation;
+using Core.Application.Rules;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Application.Pipelines.Transaction;
 
 namespace Application;
 
@@ -21,18 +23,18 @@ public static class ApplicationServiceRegistration
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        //services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
-        //services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), typeof(BaseBusinessRules));
+        services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), typeof(BaseBusinessRules));
 
-        //services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         services.AddMediatR(configuration =>
         {
             configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
 
-            //configuration.AddOpenBehavior(typeof(RequestValidationBehavior<,>));
-            //configuration.AddOpenBehavior(typeof(TransactionScopeBehavior<,>));
+            configuration.AddOpenBehavior(typeof(RequestValidationBehavior<,>));
+            configuration.AddOpenBehavior(typeof(TransactionScopeBehavior<,>));
             //configuration.AddOpenBehavior(typeof(CachingBehavior<,>));
             //configuration.AddOpenBehavior(typeof(CacheRemovingBehavior<,>));
             //configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
@@ -44,20 +46,20 @@ public static class ApplicationServiceRegistration
         return services;
     }
 
-   // public static IServiceCollection AddSubClassesOfType(
-   //    this IServiceCollection services,
-   //    Assembly assembly,
-   //    Type type,
-   //    Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null
-   //)
-   // {
-   //     var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
-   //     foreach (var item in types)
-   //         if (addWithLifeCycle == null)
-   //             services.AddScoped(item);
+    public static IServiceCollection AddSubClassesOfType(
+       this IServiceCollection services,
+       Assembly assembly,
+       Type type,
+       Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null
+   )
+    {
+        var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+        foreach (var item in types)
+            if (addWithLifeCycle == null)
+                services.AddScoped(item);
 
-   //         else
-   //             addWithLifeCycle(services, type);
-   //     return services;
-   // }
+            else
+                addWithLifeCycle(services, type);
+        return services;
+    }
 }
